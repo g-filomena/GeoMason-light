61bcf2031e2474ff2a84a4d043422eb2e46cc22c
Gabriele Filomena <gfilo@liverpool.ac.uk>
2023-09-28 13:42:03 +0100

    Update VectorLayer.java

--- src/main/java/sim/field/geo/VectorLayer.java
+++ src/main/java/sim/field/geo/VectorLayer.java
@@ -1,10 +1,13 @@
-/* 
- * Copyright 2023 by Gabriele Filomena
+/*
+ * Copyright (c) 2023 Gabriele Filomena
  * University of Liverpool, UK
- * The MIT License (MIT)
+ * 
+ * This program is free software: it can redistributed and/or modified
+ * under the terms of the GNU General Public License 3.0 as published by
+ * the Free Software Foundation.
  *
+ * See the file "LICENSE" for more information
  */
-
 package sim.field.geo;
 
 import java.net.URL;
