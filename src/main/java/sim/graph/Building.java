6dc08fe3971064defdf67e091794cf0d2169155c
Gabriele Filomena <gfilo@liverpool.ac.uk>
2023-09-28 13:42:01 +0100

    Update Building.java

--- src/main/java/sim/graph/Building.java
+++ src/main/java/sim/graph/Building.java
@@ -1,18 +1,32 @@
+/*
+ * Copyright (c) 2023 Gabriele Filomena
+ * University of Liverpool, UK
+ * 
+ * This program is free software: it can redistributed and/or modified
+ * under the terms of the GNU General Public License 3.0 as published by
+ * the Free Software Foundation.
+ *
+ * See the file "LICENSE" for more information
+ */
 package sim.graph;
 import sim.util.geo.MasonGeometry;
 
 /**
- * A class to store buildings' metainformation
+ * A class representing building metadata within a geographic information system.
+ * Each instance of this class stores information about a building, including its
+ * unique identifier, land use classification, geographic geometry, associated node
+ * in a graph, local and global landmarkness scores, and a label based on the Urban DMA categorisation.
  *
+ * This class is used to organize and manage data related to buildings in a
+ * geographical context, facilitating various spatial analysis and modeling tasks.
  */
-
 public class Building {
-
-	public int buildingID;
-	public String landUse;
-	public MasonGeometry geometry;
-	public NodeGraph node;
-	public double localLandmarkness;
-	public double globalLandmarkness;
-	public String DMA;
+    public int buildingID;           // Unique identifier for the building
+    public String landUse;           // Classification of land use for the building
+    public MasonGeometry geometry;   // Geographic geometry representing the building
+    public NodeGraph node;           // Associated node in a graph (if applicable)
+    public double localLandmarkness; // Local landmarkness score
+    public double globalLandmarkness;// Global landmarkness score
+    public String DMA;               //  Urban DMA categorisation
 }
+
