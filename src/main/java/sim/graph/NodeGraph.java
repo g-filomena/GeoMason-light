b35ba4ab369fb9cda2de93038d951697afd4ef04
Gabriele Filomena <gfilo@liverpool.ac.uk>
2024-07-08 15:56:37 +0200

    Update NodeGraph.java

--- src/main/java/sim/graph/NodeGraph.java
+++ src/main/java/sim/graph/NodeGraph.java
@@ -12,8 +12,11 @@ package sim.graph;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.planargraph.DirectedEdge;
@@ -40,8 +43,8 @@ public class NodeGraph extends Node {
 		super(pt);
 	}
 
-	public int nodeID;
-	public int regionID = -1;
+	protected int nodeID;
+	protected int regionID = -1;
 	protected MasonGeometry masonGeometry;
 	public Map<String, AttributeValue> attributes = new HashMap<>();
 	private List<EdgeGraph> edges = new ArrayList<>();
@@ -56,7 +59,7 @@ public class NodeGraph extends Node {
 
 	// only primalGraph
 	protected double centrality = Double.MAX_VALUE;
-	public List<Building> visibleBuildings2d = new ArrayList<>();
+//	public List<Building> visibleBuildings2d = new ArrayList<>();
 	public List<Building> adjacentBuildings = new ArrayList<>();
 	public List<Building> visibleBuildings3d = new ArrayList<>();
 	public String DMA = ""; // land use categorisation
@@ -76,8 +79,26 @@ public class NodeGraph extends Node {
 	 *
 	 * @return The ID of the node.
 	 */
-	public Integer getID() {
+	public int getID() {
 		return nodeID;
+	}
+
+	/**
+	 * Sets the ID of the node's region.
+	 *
+	 * @param The ID to set for the node's region.
+	 */
+	public void setRegionID(int regionID) {
+		this.regionID = regionID;
+	}
+
+	/**
+	 * Returns the ID of the node's region.
+	 *
+	 * @return The ID of the node's region.
+	 */
+	public int getRegionID() {
+		return regionID;
 	}
 
 	/**
@@ -140,14 +161,13 @@ public class NodeGraph extends Node {
 	 * edges, representing the connections from this node to its adjacent nodes.
 	 */
 	private void setEdgesNode() {
-		final List<EdgeGraph> edges = new ArrayList<>();
-		this.outEdges = new ArrayList<DirectedEdge>(this.getOutEdges().getEdges());
-		for (final DirectedEdge outEdge : outEdges) {
-			final EdgeGraph edge = (EdgeGraph) outEdge.getEdge();
-			if (!edges.contains(edge))
-				edges.add(edge);
-		}
-		this.edges = edges;
+		Set<EdgeGraph> edgeSet = new HashSet<>();
+		this.outEdges = new ArrayList<>(this.getOutEdges().getEdges());
+
+		for (DirectedEdge outEdge : outEdges)
+			edgeSet.add((EdgeGraph) outEdge.getEdge());
+
+		this.edges = new ArrayList<>(edgeSet);
 	}
 
 	/**
@@ -164,13 +184,8 @@ public class NodeGraph extends Node {
 	 * edge with this node). This method initializes the 'adjacentNodes' list.
 	 */
 	private void setAdjacentNodes() {
-		final List<NodeGraph> adjacentNodes = new ArrayList<>();
-
-		for (final EdgeGraph edge : edges) {
-			final NodeGraph opposite = (NodeGraph) edge.getOppositeNode(this);
-			adjacentNodes.add(opposite);
-		}
-		this.adjacentNodes = adjacentNodes;
+		this.adjacentNodes = edges.stream().map(edge -> (NodeGraph) edge.getOppositeNode(this))
+				.collect(Collectors.toList());
 	}
 
 	/**
@@ -179,7 +194,7 @@ public class NodeGraph extends Node {
 	 * @return The list of nodes that can be reached from this node.
 	 */
 	public List<NodeGraph> getAdjacentNodes() {
-		return adjacentNodes;
+		return new ArrayList<>(adjacentNodes);
 	}
 
 	/**
@@ -194,16 +209,10 @@ public class NodeGraph extends Node {
 		if (!gateway)
 			return null;
 
-		final List<NodeGraph> oppositeNodes = new ArrayList<>(adjacentNodes);
-		final List<Integer> adjacentRegions = new ArrayList<>();
+		List<Integer> adjacentRegions = adjacentNodes.stream().filter(opposite -> opposite.regionID != this.regionID)
+				.peek(opposite -> adjacentRegionEntries.add(opposite)).map(opposite -> opposite.regionID)
+				.collect(Collectors.toList());
 
-		for (final NodeGraph opposite : oppositeNodes) {
-			final int regionID = opposite.regionID;
-			if (regionID != this.regionID) {
-				adjacentRegions.add(regionID);
-				adjacentRegionEntries.add(opposite);
-			}
-		}
 		return adjacentRegions;
 	}
 
@@ -236,13 +245,13 @@ public class NodeGraph extends Node {
 	 */
 	public NodeGraph getDualNode(NodeGraph originNode, NodeGraph destinationNode, boolean regionBasedNavigation,
 			NodeGraph previousJunction) {
+
 		double distance = Double.MAX_VALUE;
 		NodeGraph best = null;
 		NodeGraph dualNode = null;
-		final List<EdgeGraph> edges = new ArrayList<>(this.edges);
 		double cost;
 
-		for (final EdgeGraph edge : edges) {
+		for (EdgeGraph edge : edges) {
 			if (edge.regionID == -1 && regionBasedNavigation)
 				continue;
 			dualNode = edge.getDualNode();
@@ -258,14 +267,14 @@ public class NodeGraph extends Node {
 			} else {
 				cost = GraphUtils.nodesDistance(edge.getOtherNode(this), destinationNode);
 
-				if (previousJunction != null && (previousJunction == dualNode.primalEdge.getFromNode()
+				if (previousJunction != null && (previousJunction.equals(dualNode.primalEdge.getFromNode())
 						|| previousJunction.equals(dualNode.primalEdge.getToNode())))
 					continue;
 				if (regionBasedNavigation) {
-					final List<EdgeGraph> nextEdges = new ArrayList<>(edge.getOtherNode(originNode).getEdges());
+					List<EdgeGraph> nextEdges = new ArrayList<>(edge.getOtherNode(originNode).getEdges());
 					nextEdges.remove(edge);
 					boolean bridges = true;
-					for (final EdgeGraph next : nextEdges)
+					for (EdgeGraph next : nextEdges)
 						if (next.regionID != -1)
 							bridges = false;
 					if (bridges)
@@ -307,14 +316,13 @@ public class NodeGraph extends Node {
 	public Map<NodeGraph, Double> getDualNodes(NodeGraph originNode, NodeGraph destinationNode,
 			boolean regionBasedNavigation, NodeGraph previousJunction) {
 
-		final Map<NodeGraph, Double> dualNodes = new HashMap<>();
-		NodeGraph dualNode = null;
+		Map<NodeGraph, Double> dualNodes = new HashMap<>();
 
-		for (final EdgeGraph edge : edges) {
+		for (EdgeGraph edge : edges) {
 			// gateway
 			if (edge.regionID == -1 && regionBasedNavigation)
 				continue;
-			dualNode = edge.getDualNode();
+			NodeGraph dualNode = edge.getDualNode();
 
 			if (this.equals(destinationNode)) {
 				double cost = GraphUtils.nodesDistance(edge.getOtherNode(this), originNode);
