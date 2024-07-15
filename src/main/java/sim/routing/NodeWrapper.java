0e5590b5191b1b96509fd7fd5e9fdac08b5bff5d
Gabriele Filomena <gfilo@liverpool.ac.uk>
2024-07-15 11:07:14 +0100

    Create NodeWrapper.java

---
+++ src/main/java/sim/routing/NodeWrapper.java
@@ -0,0 +1,47 @@
+package sim.routing;
+
+import org.locationtech.jts.planargraph.DirectedEdge;
+
+import sim.graph.NodeGraph;
+
+/**
+ * This class stores nodes' metainformation and supports Dikstra-based
+ * algorithms.
+ */
+public class NodeWrapper {
+
+	public NodeGraph node;
+	public NodeGraph nodeFrom;
+	public DirectedEdge directedEdgeFrom;
+	public NodeGraph commonPrimalJunction;
+	public double gx, hx, fx, landmarkness;
+	public int nodesSoFar;
+	public double pathCost, nodeLandmarkness, pathLandmarkness;
+	public NodeWrapper previousWrapper;
+
+	/**
+	 * Constructs a new NodeWrapper for the corresponding NodeGraph.
+	 *
+	 * @param node The corresponding NodeGraph.
+	 */
+	public NodeWrapper(NodeGraph node) {
+		this.node = node;
+		gx = Double.MAX_VALUE;
+		fx = Double.MAX_VALUE;
+		hx = 0.0;
+		nodeFrom = null;
+		directedEdgeFrom = null;
+		commonPrimalJunction = null;
+		pathCost = 0.0;
+		nodeLandmarkness = 0.0;
+		pathLandmarkness = 0.0;
+	}
+
+	public void reset() {
+		gx = Double.MAX_VALUE;
+		fx = Double.MAX_VALUE;
+		hx = 0.0;
+		previousWrapper = null;
+		directedEdgeFrom = null;
+	}
+}
