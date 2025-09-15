6e6f7974579a0360320cd886766a075d6aa173e8
Gabriele Filomena <gfilo@liverpool.ac.uk>
2025-09-15 11:37:59 +0100

    Update Graph.java

--- src/main/java/sim/graph/Graph.java
+++ src/main/java/sim/graph/Graph.java
@@ -25,6 +25,8 @@ import org.locationtech.jts.geom.CoordinateArrays;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.index.strtree.STRtree;
 import org.locationtech.jts.planargraph.DirectedEdge;
 import org.locationtech.jts.planargraph.PlanarGraph;
 
@@ -34,10 +36,9 @@ import sim.util.geo.MasonGeometry;
 import sim.util.geo.Utilities;
 
 /**
- * Represents a planar graph that extends the JTS (Java Topology Suite)
- * PlanarGraph class. Its basic components are {@link NodeGraph} and
- * {@link EdgeGraph}. This class provides functionalities to construct and
- * manipulate a graph from street junctions and segments.
+ * Represents a planar graph that extends the JTS (Java Topology Suite) PlanarGraph class. Its basic components are
+ * {@link NodeGraph} and {@link EdgeGraph}. This class provides functionalities to construct and manipulate a graph from
+ * street junctions and segments.
  */
 public class Graph extends PlanarGraph {
 
@@ -52,6 +53,9 @@ public class Graph extends PlanarGraph {
 	protected Set<DirectedEdge> directedEdges = new HashSet<>();
 	public Map<String, AttributeValue> attributes = new HashMap<>();
 
+	// Build spatial index once
+	STRtree index = new STRtree();
+
 	private static final GeometryFactory GEOMETRY_FACTORY = new GeometryFactory();
 
 	/**
@@ -62,9 +66,8 @@ public class Graph extends PlanarGraph {
 	}
 
 	/**
-	 * Populates the graph with nodes and edges based on street junctions and
-	 * segments. Adds LineStrings from the street segments to the graph and creates
-	 * nodes at the junctions. Also sets the 'junctions' field with the provided
+	 * Populates the graph with nodes and edges based on street junctions and segments. Adds LineStrings from the street
+	 * segments to the graph and creates nodes at the junctions. Also sets the 'junctions' field with the provided
 	 * street junctions.
 	 * 
 	 * @param streetJunctions The VectorLayer containing street junction geometries.
@@ -78,12 +81,15 @@ public class Graph extends PlanarGraph {
 
 		nodesGraph.forEach(NodeGraph::setNeighbouringComponents);
 		generateAdjacencyMatrix();
+
+		for (NodeGraph node : getNodes()) {
+			index.insert(node.masonGeometry.getGeometry().getEnvelopeInternal(), node);
+		}
 	}
 
 	/**
-	 * Populates the graph with nodes and edges based on street junctions and
-	 * segments. Adds LineStrings from the street segments to the graph and creates
-	 * nodes at the junctions. Also sets the 'junctions' field with the provided
+	 * Populates the graph with nodes and edges based on street junctions and segments. Adds LineStrings from the street
+	 * segments to the graph and creates nodes at the junctions. Also sets the 'junctions' field with the provided
 	 * street junctions.
 	 * 
 	 * @param streetJunctions The VectorLayer containing street junction geometries.
@@ -115,9 +121,8 @@ public class Graph extends PlanarGraph {
 	}
 
 	/**
-	 * Adds an EdgeGraph to the graph. This method includes the EdgeGraph in the
-	 * list of graph edges and adds it to the PlanarGraph structure. It ensures the
-	 * edge is part of the graph's edge collection and its planar graph
+	 * Adds an EdgeGraph to the graph. This method includes the EdgeGraph in the list of graph edges and adds it to the
+	 * PlanarGraph structure. It ensures the edge is part of the graph's edge collection and its planar graph
 	 * representation.
 	 *
 	 * @param edge The EdgeGraph to be added to the graph.
@@ -128,14 +133,12 @@ public class Graph extends PlanarGraph {
 	}
 
 	/**
-	 * Searches for and retrieves a NodeGraph object in the graph based on the given
-	 * coordinate. This method uses the provided coordinate to find a node within
-	 * the graph, returning the corresponding NodeGraph object if found, or null if
-	 * not found.
+	 * Searches for and retrieves a NodeGraph object in the graph based on the given coordinate. This method uses the
+	 * provided coordinate to find a node within the graph, returning the corresponding NodeGraph object if found, or
+	 * null if not found.
 	 *
 	 * @param coordinate The coordinate used to search for the node.
-	 * @return The NodeGraph object found at the specified coordinate, or null if
-	 *         not found.
+	 * @return The NodeGraph object found at the specified coordinate, or null if not found.
 	 */
 	@Override
 	public NodeGraph findNode(Coordinate coordinate) {
@@ -143,9 +146,8 @@ public class Graph extends PlanarGraph {
 	}
 
 	/**
-	 * Retrieves or creates a NodeGraph object based on the given coordinate. If a
-	 * node with the specified coordinate already exists in the graph, it is
-	 * retrieved. If not, a new NodeGraph object is created, added to the graph, and
+	 * Retrieves or creates a NodeGraph object based on the given coordinate. If a node with the specified coordinate
+	 * already exists in the graph, it is retrieved. If not, a new NodeGraph object is created, added to the graph, and
 	 * returned.
 	 *
 	 * @param coordinate The coordinate used to find or create the node.
@@ -167,9 +169,8 @@ public class Graph extends PlanarGraph {
 	}
 
 	/**
-	 * Generates the centrality map for nodes in the graph. This method computes the
-	 * centrality values for each node in the graph and stores them in a
-	 * LinkedHashMap.
+	 * Generates the centrality map for nodes in the graph. This method computes the centrality values for each node in
+	 * the graph and stores them in a LinkedHashMap.
 	 */
 	public void generateCentralityMap() {
 
@@ -180,10 +181,9 @@ public class Graph extends PlanarGraph {
 	}
 
 	/**
-	 * Generates the adjacency matrix and directed adjacency matrix for the graph.
-	 * This method populates the adjacency matrix with edges and their corresponding
-	 * directed edges, considering both the original and opposite directed edges.
-	 * The adjacency matrix stores relationships between nodes and edges.
+	 * Generates the adjacency matrix and directed adjacency matrix for the graph. This method populates the adjacency
+	 * matrix with edges and their corresponding directed edges, considering both the original and opposite directed
+	 * edges. The adjacency matrix stores relationships between nodes and edges.
 	 */
 	protected void generateAdjacencyMatrix() {
 		for (EdgeGraph edgeGraph : edgesGraph) {
@@ -252,8 +252,7 @@ public class Graph extends PlanarGraph {
 	 * Retrieves a List of graph's nodes contained within the specified Geometry.
 	 *
 	 * @param geometry The Geometry object used for containment checks.
-	 * @return A List of NodeGraph objects representing nodes that are contained
-	 *         within the specified Geometry.
+	 * @return A List of NodeGraph objects representing nodes that are contained within the specified Geometry.
 	 */
 	public List<NodeGraph> getContainedNodes(Geometry geometry) {
 
@@ -265,8 +264,7 @@ public class Graph extends PlanarGraph {
 	 * Retrieves a List of graph's edges contained within the specified Geometry.
 	 *
 	 * @param geometry The Geometry object used for containment checks.
-	 * @return A List of EdgeGraph objects representing edges that are contained
-	 *         within the specified Geometry.
+	 * @return A List of EdgeGraph objects representing edges that are contained within the specified Geometry.
 	 */
 	public List<EdgeGraph> getContainedEdges(Geometry geometry) {
 		return edgesGraph.parallelStream().filter(edge -> geometry.contains(edge.masonGeometry.geometry))
@@ -274,42 +272,38 @@ public class Graph extends PlanarGraph {
 	}
 
 	/**
-	 * Retrieves the edge between two nodes, if it exists in the graph's adjacency
-	 * matrix.
+	 * Retrieves the edge between two nodes, if it exists in the graph's adjacency matrix.
 	 *
 	 * @param fromNode The source node of the edge.
 	 * @param toNode   The target node of the edge.
-	 * @return The EdgeGraph object representing the edge between the specified
-	 *         source and target nodes, or null if no such edge exists.
+	 * @return The EdgeGraph object representing the edge between the specified source and target nodes, or null if no
+	 *         such edge exists.
 	 */
 	public EdgeGraph getEdgeBetween(NodeGraph fromNode, NodeGraph toNode) {
 		return adjacencyMatrix.get(new Pair<>(fromNode, toNode));
 	}
 
 	/**
-	 * Retrieves the directed edge between two nodes, if it exists in the graph's
-	 * directed adjacency matrix.
+	 * Retrieves the directed edge between two nodes, if it exists in the graph's directed adjacency matrix.
 	 *
 	 * @param fromNode The source node of the directed edge.
 	 * @param toNode   The target node of the directed edge.
-	 * @return The DirectedEdge object representing the directed edge between the
-	 *         specified source and target nodes, or null if no such edge exists.
+	 * @return The DirectedEdge object representing the directed edge between the specified source and target nodes, or
+	 *         null if no such edge exists.
 	 */
 	public DirectedEdge getDirectedEdgeBetween(NodeGraph fromNode, NodeGraph toNode) {
 		return adjacencyMatrixDirected.get(new Pair<>(fromNode, toNode));
 	}
 
 	/**
-	 * Filters a LinkedHashMap of centrality values to include only nodes that are
-	 * present in the given list of nodes. This method retains entries in the
-	 * original map for nodes that are also present in the filter list and returns
+	 * Filters a LinkedHashMap of centrality values to include only nodes that are present in the given list of nodes.
+	 * This method retains entries in the original map for nodes that are also present in the filter list and returns
 	 * the filtered map.
 	 *
-	 * @param map    The LinkedHashMap containing centrality values associated with
-	 *               nodes.
+	 * @param map    The LinkedHashMap containing centrality values associated with nodes.
 	 * @param filter The list of nodes to use for filtering the map.
-	 * @return A filtered LinkedHashMap containing centrality values for nodes
-	 *         present in both the original map and the filter list.
+	 * @return A filtered LinkedHashMap containing centrality values for nodes present in both the original map and the
+	 *         filter list.
 	 */
 	public static Map<NodeGraph, Double> filterCentralityMap(Map<NodeGraph, Double> map, List<NodeGraph> filter) {
 
@@ -318,15 +312,12 @@ public class Graph extends PlanarGraph {
 	}
 
 	/**
-	 * Retrieves salient nodes from the graph based on a specified percentile
-	 * threshold of centrality values. This method calculates the boundary value for
-	 * centrality based on the specified percentile and filters nodes with
+	 * Retrieves salient nodes from the graph based on a specified percentile threshold of centrality values. This
+	 * method calculates the boundary value for centrality based on the specified percentile and filters nodes with
 	 * centrality values exceeding that threshold.
 	 *
-	 * @param percentile The percentile threshold for selecting salient nodes (e.g.,
-	 *                   0.75 for the top 25% centrality).
-	 * @return A map of salient nodes and their centrality values exceeding the
-	 *         specified percentile threshold.
+	 * @param percentile The percentile threshold for selecting salient nodes (e.g., 0.75 for the top 25% centrality).
+	 * @return A map of salient nodes and their centrality values exceeding the specified percentile threshold.
 	 */
 	public Map<NodeGraph, Double> getSalientNodes(double percentile) {
 
@@ -341,19 +332,16 @@ public class Graph extends PlanarGraph {
 	}
 
 	/**
-	 * Retrieves salient nodes within the spatial proximity of two reference nodes
-	 * based on a specified percentile threshold. This method calculates the
-	 * smallest enclosing circle around the reference nodes and filters nodes within
-	 * that spatial area. It then filters nodes based on their centrality values and
-	 * returns a map of salient nodes exceeding the specified percentile threshold.
+	 * Retrieves salient nodes within the spatial proximity of two reference nodes based on a specified percentile
+	 * threshold. This method calculates the smallest enclosing circle around the reference nodes and filters nodes
+	 * within that spatial area. It then filters nodes based on their centrality values and returns a map of salient
+	 * nodes exceeding the specified percentile threshold.
 	 *
 	 * @param node       The first reference node for spatial calculations.
 	 * @param otherNode  The second reference node for spatial calculations.
-	 * @param percentile The percentile threshold for selecting salient nodes (e.g.,
-	 *                   0.75 for the top 25% centrality).
-	 * @return A map of salient nodes and their centrality values within the spatial
-	 *         proximity of the reference nodes, exceeding the specified percentile
-	 *         threshold.
+	 * @param percentile The percentile threshold for selecting salient nodes (e.g., 0.75 for the top 25% centrality).
+	 * @return A map of salient nodes and their centrality values within the spatial proximity of the reference nodes,
+	 *         exceeding the specified percentile threshold.
 	 */
 	public Map<NodeGraph, Double> getSalientNodesWithinSpace(NodeGraph node, NodeGraph otherNode, double percentile) {
 
@@ -379,9 +367,8 @@ public class Graph extends PlanarGraph {
 	}
 
 	/**
-	 * Returns a List of nodes in the graph whose distance from the given node falls
-	 * within the specified range and have centrality values above the specified
-	 * percentile.
+	 * Returns a List of nodes in the graph whose distance from the given node falls within the specified range and have
+	 * centrality values above the specified percentile.
 	 *
 	 * @param node       The reference node.
 	 * @param lowerLimit The minimum distance from the reference node.
@@ -409,15 +396,12 @@ public class Graph extends PlanarGraph {
 	}
 
 	/**
-	 * Retrieves edges within the spatial proximity of two nodes. This method
-	 * calculates a spatial buffer based on the distance between two nodes and
-	 * retrieves edges that fall within the combined spatial buffer of the two
-	 * nodes.
+	 * Retrieves edges within the spatial proximity of two nodes. This method calculates a spatial buffer based on the
+	 * distance between two nodes and retrieves edges that fall within the combined spatial buffer of the two nodes.
 	 *
 	 * @param node      The first node used for spatial reference.
 	 * @param otherNode The second node used for spatial reference.
-	 * @return A List of edges that are located within the combined spatial buffer
-	 *         of the two nodes.
+	 * @return A List of edges that are located within the combined spatial buffer of the two nodes.
 	 */
 	public List<EdgeGraph> edgesInNodesSpace(NodeGraph node, NodeGraph otherNode) {
 
@@ -429,9 +413,8 @@ public class Graph extends PlanarGraph {
 	}
 
 	/**
-	 * Filters out nodes belonging to a specified region from a List of nodes.
-	 * Returns a new list containing nodes not associated with the specified region
-	 * ID.
+	 * Filters out nodes belonging to a specified region from a List of nodes. Returns a new list containing nodes not
+	 * associated with the specified region ID.
 	 *
 	 * @param nodes    The original list of nodes to filter.
 	 * @param regionID The ID of the region to exclude nodes from.
@@ -444,12 +427,33 @@ public class Graph extends PlanarGraph {
 	/**
 	 * Finds a node in the graph that matches the given node's geometry coordinates.
 	 *
-	 * @param nodeGraph the node whose geometry coordinates will be used for the
-	 *                  search
-	 * @return the node in the graph that matches the given node's geometry
-	 *         coordinates
+	 * @param nodeGraph the node whose geometry coordinates will be used for the search
+	 * @return the node in the graph that matches the given node's geometry coordinates
 	 */
 	public NodeGraph findNode(NodeGraph nodeGraph) {
 		return findNode(nodeGraph.getMasonGeometry().geometry.getCoordinate());
+	}
+
+	/**
+	 * Retrieves all {@link NodeGraph} objects whose geometries are contained within the specified polygon.
+	 *
+	 * The method first queries the spatial index with the polygon's bounding envelope to retrieve candidate nodes. If
+	 * no candidates are found, an empty list is returned. Otherwise, a precise containment check is performed against
+	 * the given polygon to filter only those nodes whose geometry is strictly contained within it.
+	 *
+	 * @param polygon the {@link Polygon} defining the search area; must not be null
+	 * @return a list of {@link NodeGraph} instances located inside the polygon; the list is empty if no nodes fall
+	 *         within the polygon
+	 */
+	public List<NodeGraph> getNodesWithinPolygon(Polygon polygon) {
+		List<NodeGraph> nodesInPolygon = new ArrayList<>();
+		@SuppressWarnings("unchecked")
+		List<NodeGraph> candidates = (List<NodeGraph>) (List<?>) index.query(polygon.getEnvelopeInternal());
+
+		if (candidates.isEmpty())
+			return nodesInPolygon;
+
+		return candidates.stream().filter(node -> polygon.contains(node.masonGeometry.getGeometry()))
+				.collect(Collectors.toList());
 	}
 }
